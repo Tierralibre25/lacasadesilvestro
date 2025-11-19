fb60dd6d5e9bc3b9eb72e5172a89149fb47b1967
Tierralibre25 <info@cooperativatierralibre.es>
2025-11-19 13:35:40 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
 // pages/index.tsx
+import Head from 'next/head'
 import Hero from '../components/Hero'
 import Stats from '../components/Stats'
 import Timeline from '../components/Timeline'
 import Conocenos from '../components/Conocenos'
-import Support from '../components/Support' 
+import Support from '../components/Support'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>La Casa de Silvestro · Refugio felino en Villalonga</title>
+        <meta
+          name="description"
+          content="Refugio felino sin jaulas en Villalonga (Valencia). Cuidamos a más de 20 gatos callejeros ofreciéndoles comida, refugio y atención veterinaria. Ayúdanos con tu donación o colaboración."
+        />
+      </Head>
+
       <Hero />
       <Stats />
       <Timeline />
@@ -16,3 +25,4 @@ export default function Home() {
     </>
   )
 }
+
