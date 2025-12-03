eca7f83514130f6c76f991004d132cd04d0d58a2
Tierralibre25 <info@cooperativatierralibre.es>
2025-12-03 20:01:01 +0100

    Add blog link to the homepage
    Added a link to the blog section in the homepage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Stats from '../components/Stats'
 import Timeline from '../components/Timeline'
 import Conocenos from '../components/Conocenos'
 import Support from '../components/Support'
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,7 +17,13 @@ export default function Home() {
           content="Refugio felino sin jaulas en Villalonga (Valencia). Cuidamos a más de 20 gatos callejeros ofreciéndoles comida, refugio y atención veterinaria. Ayúdanos con tu donación o colaboración."
         />
       </Head>
-
+       <div className="text-center mt-16 mb-12">
+  <Link href="/blog">
+    <a className="inline-block bg-green-700 text-white text-xl font-semibold px-10 py-4 rounded-xl shadow-lg hover:bg-green-800 transition">
+      Nuestro Blog
+    </a>
+  </Link>
+</div>
       <Hero />
       <Stats />
       <Timeline />
@@ -25,4 +32,5 @@ export default function Home() {
     </>
   )
 }
+
 
